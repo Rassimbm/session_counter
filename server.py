a9825ed4ee3206cbfdf700fae6a0746aa3ebd665
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-04 19:42:57 -0600

    updated server.py: completed task 1

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.secret_key = "8891techa0128"
+
+@app.route("/")
+def index():
+    if "visit_count" not in session:
+        session["visit_count"] = 1
+    else:
+        session["visit_count"] += 1
+    return render_template("index.html", visits_count = session["visit_count"])
 
 if __name__ == "__main__":
     app.run(debug=True)
