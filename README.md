ebb0ca7d005d2d838573bdbc6cd3e8ef4c8bf639
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-02-07 22:47:13 -0600

    Task 4, 5 and 6

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 2. Add a "/destroy_session" route that clears the session and redirects to the root route. Test it.
 
 3. NINJA BONUS: Add a +2 button underneath the counter and a new route that will increment the counter by 2
+
+4. NINJA BONUS: Add a Reset button to reset the counter
+
+5. SENSEI BONUS: Add a form that allows the user to specify the increment of the counter and have the counter increment accordingly
+
+6. SENSEI BONUS: Adjust your code to display both how many times the user has actually visited the page, as well as the value of the counter, given the above functionality
--- server.py
+++ server.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "8891techa0128"
 
 @app.route("/")
 def index():
-    if "visit_count" not in session:
-        session["visit_count"] = 1
+    if "visit" and "counter" not in session:
+        session["visit"] = 0
+        session["counter"] = 0
     else:
-        session["visit_count"] += 1
-    return render_template("index.html", visits_count = session["visit_count"])
+        session["visit"] += 1
+        session["counter"] += 1
 
-@app.route("/destroy-session")
-def clear_session():
-    # session.pop("visit_count")
-    session.clear()
+    return render_template("index.html", visit = session["visit"], counter = session["counter"])
+
+@app.route("/reset-counter")
+def reset_visits():
+    session["visit"] = 0
     return redirect("/")
 
-@app.route("/increment")
-def add_2_visits():
-    session['visit_count'] = session.get('visit_count', 1) + 1
+@app.route("/increment-by-two")
+def increment_by_two():
+    if "visit" and "counter" in session:
+        session['visit'] += 1
+        session["counter"] += 1
+    return redirect("/")
+
+@app.route("/increment-by-val", methods = ["POST"])
+def increment_by_val():
+    increment_val = int(request.form["increment_value"])
+    session["counter"] += increment_val
+    return redirect("/")
+
+@app.route("/reset-session")
+def clear_session():
+    # session.pop("visits")
+    session.clear()
     return redirect("/")
 
 if __name__ == "__main__":
